ea771e7c39cdce7268e7384529c619399e9e157e
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2020-04-02 20:18:13 +0500

    Set up routes for Language model controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :languages
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
